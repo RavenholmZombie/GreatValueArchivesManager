6a3d2478fbd17f359c5758bbc467dbd7b4c227ef
Christopher <supersoftware1234@gmail.com>
2026-09-05 18:38:10 -0400

    Starting UX

---
+++ frmLogin.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace GreatValueArchivesManager
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            txtBoxUser = new ReaLTaiizor.Controls.CrownTextBox();
+            label1 = new Label();
+            txtBoxPass = new ReaLTaiizor.Controls.CrownTextBox();
+            label2 = new Label();
+            label3 = new Label();
+            btnLogin = new ReaLTaiizor.Controls.CrownButton();
+            btnCPanel = new ReaLTaiizor.Controls.CrownButton();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackgroundImage = Properties.Resources.aurora;
+            panel1.BackgroundImageLayout = ImageLayout.None;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(433, 100);
+            panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.None;
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.banner;
+            pictureBox1.Location = new Point(69, 25);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(295, 50);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // txtBoxUser
+            // 
+            txtBoxUser.BackColor = Color.FromArgb(69, 73, 74);
+            txtBoxUser.BorderStyle = BorderStyle.FixedSingle;
+            txtBoxUser.ForeColor = Color.FromArgb(220, 220, 220);
+            txtBoxUser.Location = new Point(69, 174);
+            txtBoxUser.Name = "txtBoxUser";
+            txtBoxUser.Size = new Size(295, 23);
+            txtBoxUser.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(89, 122);
+            label1.Name = "label1";
+            label1.Size = new Size(254, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Login to the Great Value Archives FTP to begin.";
+            // 
+            // txtBoxPass
+            // 
+            txtBoxPass.BackColor = Color.FromArgb(69, 73, 74);
+            txtBoxPass.BorderStyle = BorderStyle.FixedSingle;
+            txtBoxPass.ForeColor = Color.FromArgb(220, 220, 220);
+            txtBoxPass.Location = new Point(69, 229);
+            txtBoxPass.Name = "txtBoxPass";
+            txtBoxPass.Size = new Size(295, 23);
+            txtBoxPass.TabIndex = 4;
+            txtBoxPass.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(69, 156);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 15);
+            label2.TabIndex = 5;
+            label2.Text = "FTP Username:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(69, 211);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 15);
+            label3.TabIndex = 6;
+            label3.Text = "FTP Password:";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(272, 277);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Padding = new Padding(5);
+            btnLogin.Size = new Size(92, 27);
+            btnLogin.TabIndex = 7;
+            btnLogin.Text = "Login";
+            // 
+            // btnCPanel
+            // 
+            btnCPanel.Location = new Point(69, 277);
+            btnCPanel.Name = "btnCPanel";
+            btnCPanel.Padding = new Padding(5);
+            btnCPanel.Size = new Size(92, 27);
+            btnCPanel.TabIndex = 8;
+            btnCPanel.Text = "Open CPanel";
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(62, 62, 66);
+            ClientSize = new Size(433, 340);
+            Controls.Add(btnCPanel);
+            Controls.Add(btnLogin);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txtBoxPass);
+            Controls.Add(label1);
+            Controls.Add(txtBoxUser);
+            Controls.Add(panel1);
+            ForeColor = Color.White;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private ReaLTaiizor.Controls.CrownTextBox txtBoxUser;
+        private Label label1;
+        private ReaLTaiizor.Controls.CrownTextBox txtBoxPass;
+        private Label label2;
+        private Label label3;
+        private ReaLTaiizor.Controls.CrownButton btnLogin;
+        private ReaLTaiizor.Controls.CrownButton btnCPanel;
+    }
+}
--- frmManager.Designer.cs
+++ frmManager.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmManager));
+            menuStrip = new ReaLTaiizor.Controls.CrownMenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            viewToolStripMenuItem = new ToolStripMenuItem();
+            toolsToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            pnlActions = new Panel();
+            pnlSidebar = new Panel();
+            pnlViewer = new Panel();
+            listView1 = new ListView();
+            menuStrip.SuspendLayout();
+            pnlViewer.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            menuStrip.BackColor = Color.FromArgb(60, 63, 65);
+            menuStrip.ForeColor = Color.FromArgb(220, 220, 220);
+            menuStrip.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, viewToolStripMenuItem, toolsToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Padding = new Padding(3, 2, 0, 2);
+            menuStrip.Size = new Size(800, 24);
+            menuStrip.TabIndex = 0;
+            menuStrip.Text = "crownMenuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            fileToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            editToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(39, 20);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            viewToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            viewToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            viewToolStripMenuItem.Size = new Size(44, 20);
+            viewToolStripMenuItem.Text = "View";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            toolsToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            toolsToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            toolsToolStripMenuItem.Size = new Size(47, 20);
+            toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            helpToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(44, 20);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // pnlActions
+            // 
+            pnlActions.BorderStyle = BorderStyle.FixedSingle;
+            pnlActions.Dock = DockStyle.Bottom;
+            pnlActions.Location = new Point(0, 440);
+            pnlActions.Name = "pnlActions";
+            pnlActions.Size = new Size(800, 58);
+            pnlActions.TabIndex = 1;
+            // 
+            // pnlSidebar
+            // 
+            pnlSidebar.BackColor = Color.FromArgb(45, 45, 48);
+            pnlSidebar.BorderStyle = BorderStyle.FixedSingle;
+            pnlSidebar.Dock = DockStyle.Left;
+            pnlSidebar.Location = new Point(0, 24);
+            pnlSidebar.Name = "pnlSidebar";
+            pnlSidebar.Size = new Size(200, 416);
+            pnlSidebar.TabIndex = 2;
+            // 
+            // pnlViewer
+            // 
+            pnlViewer.BorderStyle = BorderStyle.FixedSingle;
+            pnlViewer.Controls.Add(listView1);
+            pnlViewer.Dock = DockStyle.Fill;
+            pnlViewer.Location = new Point(200, 24);
+            pnlViewer.Name = "pnlViewer";
+            pnlViewer.Size = new Size(600, 416);
+            pnlViewer.TabIndex = 3;
+            // 
+            // listView1
+            // 
+            listView1.BackColor = Color.FromArgb(30, 30, 30);
+            listView1.BorderStyle = BorderStyle.FixedSingle;
+            listView1.Dock = DockStyle.Fill;
+            listView1.ForeColor = Color.White;
+            listView1.Location = new Point(0, 0);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(598, 414);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // frmManager
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            BackColor = Color.FromArgb(62, 62, 66);
+            ClientSize = new Size(800, 498);
+            Controls.Add(pnlViewer);
+            Controls.Add(pnlSidebar);
+            Controls.Add(pnlActions);
+            Controls.Add(menuStrip);
+            ForeColor = Color.White;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip;
+            Name = "frmManager";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Great Value Archives Manager";
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
+            pnlViewer.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ReaLTaiizor.Controls.CrownMenuStrip menuStrip;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem viewToolStripMenuItem;
+        private ToolStripMenuItem toolsToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private Panel pnlActions;
+        private Panel pnlSidebar;
+        private Panel pnlViewer;
+        private ListView listView1;
     }
 }
