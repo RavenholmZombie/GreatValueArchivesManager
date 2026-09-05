008a4046bb4348a0072e8dae5588397487701c5a
Christopher <supersoftware1234@gmail.com>
2026-09-05 18:41:42 -0400

    Start application at login form

--- Program.cs
+++ Program.cs
@@ -8,10 +8,8 @@ namespace GreatValueArchivesManager
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frmManager());
+            Application.Run(new frmLogin());
         }
     }
 }
